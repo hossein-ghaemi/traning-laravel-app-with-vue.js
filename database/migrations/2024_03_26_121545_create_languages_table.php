dc3b669f27c00127fb8477722dcc79c353e8d342
hossein-ghaemi <jamshid.gh78@gmail.com>
2024-03-28 16:08:49 +0330

    add dashboard

---
+++ database/migrations/2024_03_26_121545_create_languages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('languages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',15);
+            $table->string('nickname',2)->index();
+            $table->boolean('status')->default(0)->index();
+            $table->boolean('archive')->default(0)->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('languages');
+    }
+};
