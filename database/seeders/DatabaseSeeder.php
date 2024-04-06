6e8f335a09beca2e8208d14502c4f46106b9a644
Hossein Ghaemi <h.ghaemi256@gmail.com>
2024-04-06 15:53:28 +0330

    fix bug in order of database seeder caller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call([
-            UserSeeder::class,
             RoleSeeder::class,
+            UserSeeder::class,
             AccessSeeder::class
         ]);
     }
